a21b850c14677fed79e72cc56fe42c1e874bd6bf
Thalitalima23 <thalitalima23@gmail.com>
2025-10-22 16:22:49 -0300

    :sparkles: CRUD da classe Exercicios

---
+++ src/main/java/com/maventech/shapefit/model/Exercicios.java
@@ -0,0 +1,62 @@
+package com.maventech.shapefit.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+	@Entity
+	@Table(name = "tb_exercicios")
+	public class Exercicios {
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private long id;
+		
+		@NotBlank(message = "O nome do exercício é obrigatório")
+		@Size(min=3 , max = 100, message = "O nome deve conter entre 3 e 100 caracteres!")
+		private String nome;
+		
+		@NotBlank(message = "A descrição é obrigatória!")
+	    @Size(min = 10, max = 1000, message = "A descrição deve conter entre 10 e 1000 caracteres!")
+	    private String descricao;
+		
+		
+		// Getters e Setters//
+
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getDescricao() {
+			return descricao;
+		}
+
+		public void setDescricao(String descricao) {
+			this.descricao = descricao;
+		}
+
+		
+		
+		
+		
+		
+}
