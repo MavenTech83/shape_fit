e3e410fc9be5ac8dea747ddb86f9de9f67af8cb3
Ana Lemos <carol1692@hotmail.com>
2025-10-22 12:05:40 -0300

    commit inicial

---
+++ src/main/java/com/maventech/shapefit/ShapefitApplication.java
@@ -0,0 +1,13 @@
+package com.maventech.shapefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapefitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapefitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maventech/shapefit/ShapefitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maventech.shapefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShapefitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
